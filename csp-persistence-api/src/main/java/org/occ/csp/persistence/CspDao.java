4778f2c659120b4e38afb32ded7cbcc4bc6cd7fe
Mark Wong <yefang.wong@gmail.com>
2015-10-20 06:22:37 +0800

    1.add some interfaces. 2.add DB support MS SqlServer.

--- csp-persistence-api/src/main/java/org/occ/csp/persistence/CspDao.java
+++ csp-persistence-api/src/main/java/org/occ/csp/persistence/CspDao.java
@@ -2,6 +2,7 @@ package org.occ.csp.persistence;
 
 import java.util.List;
 
+import org.occ.csp.domain.ActivatedCardInfo;
 import org.occ.csp.domain.ChurchMember;
 import org.occ.csp.domain.Fellowship;
 import org.occ.csp.domain.Footprint;
@@ -20,4 +21,11 @@ public interface CspDao {
 	public void saveFootprint(Footprint footprint);
 	public int findMaxFootprintId();
 	public List<Footprint> findFootprintsByMemberId(String memberId);
+	public String findChurchMemberSidByName(String name);
+	public void insertActivatedCardInfo(String memberSid, String cardNum);
+	public void insertActivatedCardInfo(String OCCId, String memberSid, String cardNumber);
+	public String findOccIdFromCardNum(String cardNum);
+	public void deleteCard(String cardNumber);
+	public List<ActivatedCardInfo> findActicatedCardInfoByMemberSid(String memberSid);
+	public List<ActivatedCardInfo> findAllActivatedCardList();
 }
