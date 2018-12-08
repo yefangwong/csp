09253303400f8dfe26c7c08125bca094e729c1fd
yf <yefang.wong@gmail.com>
2018-12-08 21:33:15 +0800

    Update csp-ois/src/test/java/com/yf/test/selenium/TestStandaloneSeleniumServer.java
    新增 firefox 測試環境。

--- csp-ois/src/test/java/com/yf/test/selenium/TestStandaloneSeleniumServer.java
+++ csp-ois/src/test/java/com/yf/test/selenium/TestStandaloneSeleniumServer.java
@@ -14,6 +14,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TestStandaloneSeleniumServer {
+	@Test
 	public void testFirefoxDriver() throws MalformedURLException {
 		this.execute(DesiredCapabilities.firefox());
 	}
